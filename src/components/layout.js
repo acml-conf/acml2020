4d2b4535efcb3477c9facda80fe5034fd1b1765e
heytitle <pat.chormai@gmail.com>
2020-01-20 17:40:10 +0100

    fix footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import { config } from "@fortawesome/fontawesome-svg-core";
 
+import { DESKTOP_MIN_WIDTH, media } from "../style"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -46,12 +47,20 @@ const Layout = ({ children }) => {
       </div>
       <footer css={{background: `black`, color: `white`, padding: `2em 0`}}>
         <div css={{maxWidth: 1024, margin: `0 auto`, padding: "0 10px"}}>
-          <div css={{fontSize: `1em`, marginBottom: `10px`}}>
+          <div css={{
+              fontSize: `1em`, marginBottom: `10px`,
+              [media(DESKTOP_MIN_WIDTH)]: {
+                float: `right`,
+                clear: `both`,
+              }
+            }}>
             <b>Follow us:</b> <BeautifulLink to="https://github.com/heytitle/acml2020" color="white"><FontAwesomeIcon icon={faGithub}/></BeautifulLink> 
           </div>
         © {new Date().getFullYear()} ACML, Built with
         {` `}
-        <BeautifulLink color="white" to="https://www.gatsbyjs.org">Gatsby</BeautifulLink>
+        <BeautifulLink color="white" to="https://www.gatsbyjs.org">Gatsby</BeautifulLink>.
+        <br/>
+        Layout and design inspired by <BeautifulLink color="white" to="https://acl2020.org">ACL2020's webiste</BeautifulLink>.
         </div>
       </footer>
     </div>
