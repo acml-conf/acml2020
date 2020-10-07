84222c4848d3955239548ca3e0078647eb44f4c6
Pat Chormai <pat.chormai@gmail.com>
2020-10-07 22:46:13 +0200

    add papers

--- src/content/program/accepted-papers.md
+++ src/content/program/accepted-papers.md
@@ -8,6 +8,6 @@ useMDX: true
 
 import AcceptedPapersList from "../../templates/acceptedPapersList";
 
-This year's accepted papers are now available on [PMLR Volume 129](http://proceedings.mlr.press/v129/).
+🗄 Conference track papers are now available in [PMLR Volume 129](http://proceedings.mlr.press/v129/).
 
 <AcceptedPapersList/>
--- src/templates/acceptedPapersList.js
+++ src/templates/acceptedPapersList.js
@@ -1,33 +1,45 @@
 import React from "react"
 
-const papers = [
-    {
-        name: "Paper 1",
-        authors: "Mr. A, Mr.B; Mr. C"
-    },
-    {
-        name: "Paper 2",
-        authors: "Mr. A, Mr.B; Mr. C"
-    },
-    {
-        name: "Paper 3",
-        authors: "Mr. A, Mr.B; Mr. C"
-    },
-]
+const papers = require("../content/acml-papers.json")
+    .map(p => {
+        return {
+            ...p,
+            title: p.title.replace(/[\{\}]/g, ``),
+        }
+    })
+    .sort( (a, b) => a.title.localeCompare(b.title))
+
+
+const Paper = ({title, authors}) => {
+    return <li key={title}>
+        <b>{title}</b><br/>
+        {authors}
+    </li>
+}
 
 const AcceptedPapersList = () => {
+    console.log(papers)
+    const journalPapers = papers.filter(p => p.crossref === "Journal Track")
+    const confPapers = papers.filter(p => p.crossref === "acml20")
+
     return <div>
-
-        In total, we have accepted {papers.length} papers out of XXX submissions.
+        <h3 css={{marginBottom: 0}}>Journal Track ({journalPapers.length} papers)</h3>
         <br/>
-
         <ul css={{listStyle: `none`, padding: 0, margin: 0}}>
-            {papers.map(paper => {
-                return <li key={paper.name}>
-                    <b>{paper.name}</b><br/>
-                    {paper.authors}
-                </li>
-            })}
+            {
+                journalPapers.map(
+                    paper => <Paper title={paper.title} authors={paper.author}/>
+                )
+            }
+        </ul><br/>
+        <h3 css={{marginBottom: 0}}>Conference Track ({confPapers.length} papers)</h3>
+        <br/>
+        <ul css={{listStyle: `none`, padding: 0, margin: 0}}>
+            {
+                confPapers.map(
+                    paper => <Paper title={paper.title} authors={paper.author}/>
+                )
+            }
         </ul>
     </div>
 }
